ab80147a2623c1f1b986e3ecb83b3ea5c5297907
kogarashi <brayanarellano1605@gmail.com>
2023-11-24 09:36:16 -0500

    Backend: Start command & fix database url in production

--- backend/configs/constants.js
+++ backend/configs/constants.js
@@ -8,7 +8,7 @@ const DB_HOST = process.env.DB_HOST
 const DB_PORT = process.env.DB_PORT
 const DB_USER = process.env.DB_USER
 const DB_PSWD = process.env.DB_PSWD
-const DB_URI = process.env.NODE_ENV === 'production' ? `mongodb://${DB_USER}:${DB_PSWD}@${DB_HOST}:${DB_PORT}/agenda` : `mongodb://${DB_HOST}:${DB_PORT}/agenda`
+const DB_URI = process.env.NODE_ENV === 'production' ? `mongodb+srv://${DB_USER}:${DB_PSWD}@${DB_HOST}/agenda` : `mongodb://${DB_HOST}:${DB_PORT}/agenda`
 const JWT_SECRET = process.env.JWT_SECRET
 const DB_ENCRYPTION_KEY = process.env.DB_ENCRYPTION_KEY
 
