888f3f6f8c1a2faae0aeaa31c626249735c8df56
Brayan Arellano <brayanarellano1605@gmail.com>
2024-05-29 17:58:52 -0500

    manifest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,40 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({registerType: 'autoUpdate'})
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Agenda ACM',
+        short_name: 'AgendaACM',
+        description: 'Una agenda desarrollada en nodejs y react',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })
   ],
 })
